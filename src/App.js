fa070dc6558d383a37cfe4c6f3e8a66223c0b124
chetbaby <chet.hay@gmail.com>
2019-08-20 20:19:08 -0700

    cleaned app component

--- src/App.js
+++ src/App.js
@@ -1,29 +1,11 @@
-import React, { useState, useEffect } from 'react';
+import React from 'react';
 import './App.css';
-import ReactJson from 'react-json-view';
-import Axios from 'axios';
+import Navbar from './Navbar';
 
 function App() {
-  const [list, setList] = useState('');
-  const [loading, setLoading] = useState(false);
-  useEffect(() => {
-    setLoading(true);
-    Axios.get('/api')
-      .then(result => {
-        setList(result.data);
-        setLoading(false);
-      })
-      .catch(err => console.error(err));
-  }, []);
-
   return (
     <div>
-      <h1>hello world</h1>
-      {loading ? (
-        <span>Loading...</span>
-      ) : (
-        <ReactJson src={list} theme="hop scotch" />
-      )}
+      <Navbar />
     </div>
   );
 }
